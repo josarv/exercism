366e4e6702d3cd546a9ff969a9961f267942f939
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-09-26 22:12:56 +0300

    c 2

---
+++ c/2-armstrong-numbers/armstrong-numbers.c
@@ -0,0 +1,20 @@
+#include <math.h>
+
+#include "armstrong-numbers.h"
+
+bool is_armstrong_number(int candidate)
+{
+    int armstrong_sum = 0, copy_candidate = candidate, digits = 0;
+    while(copy_candidate > 0)
+    {
+        copy_candidate /= 10;
+        digits++;
+    }
+    copy_candidate = candidate;
+    while(candidate > 0)
+    {
+        armstrong_sum += pow(candidate % 10, digits);
+        candidate /= 10;
+    }
+    return copy_candidate == armstrong_sum;
+}
