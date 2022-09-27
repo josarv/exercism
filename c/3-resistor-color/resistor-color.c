eedc1f70f50c21057d89942347899e6f5543137b
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-09-27 13:48:17 +0300

    c 3

---
+++ c/3-resistor-color/resistor-color.c
@@ -0,0 +1,13 @@
+#include "resistor_color.h"
+
+static resistor_band_t color_list[] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+
+resistor_band_t color_code(resistor_band_t color)
+{
+    return color;
+}
+
+resistor_band_t *colors(void)
+{
+    return (resistor_band_t *)&color_list;
+}
